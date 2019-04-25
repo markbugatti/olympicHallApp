eedc59baa822734df944b0202ab398557a1605dc
markbugatti <supermark11@gmail.com>
2019-04-25 11:31:13 +0300

    images folder was added

--- app/scripts.js
+++ app/scripts.js
@@ -1,7 +1,7 @@
 var myFullpage = new fullpage('#fullpage', {
     licenseKey: '981093A2-2C134508-B175A706-5E3F2241',
     // sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
-    anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage', '5thPage'],
+    anchors: ['firstPage', 'secondPage', '3rdPage'],
     // menu: '#menu',
     slidesNavigation: true,
     // responsiveHeight: 330,
